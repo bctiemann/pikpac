14bc998169728ae737adc2b5c4a97af0d9e5d98f
Brian Tiemann <btman@mac.com>
2019-06-07 08:43:33 -0400

    REST API basics

---
+++ api/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+
+from products.models import Product
+from . import serializers
+
+class LoginView(APIView):
+
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+        response = {}
+        print(request.data)
+        if not ('email' in request.data and ('password' in request.data or 'confirm_hashkey' in request.data)):
+            raise ValidationError('Invalid request.')
+
+        if 'password' in request.data:
+            user = authenticate(username=request.data['email'], password=request.data['password'])
+
+            if not user:
+                raise ValidationError('Invalid login credentials.')
+
+            if not user.is_active:
+                raise ValidationError('User account has been disabled.')
+
+            if not user.is_confirmed:
+                raise ValidationError('User account has not been confirmed.')
+            print('Login via password')
+
+        else:
+            try:
+                user = User.objects.get(email=request.data.get('email'),
+                                        confirm_hashkey=request.data.get('confirm_hashkey'))
+            except User.DoesNotExist:
+                raise ValidationError('Invalid hash key for this account.')
+            print('Login via hashkey')
+            user.confirm_hashkey = None
+            user.save()
+
+        auth_token, is_created = Token.objects.get_or_create(user=user)
+        response['email'] = user.email
+        response['token'] = auth_token.key
+
+        login(request, user)
+
+        return Response(response)
+
+
+class LogoutView(APIView):
+
+    def post(self, request, format=None):
+        response = {}
+        request.user.auth_token.delete()
+
+        logout(request)
+
+        return Response(response)
+
+
+class MeView(APIView):
+
+    def get(self, request):
+        response = {
+            'data': {
+                'id': self.request.user.id,
+                'username': self.request.user.email,
+                'email': self.request.user.email,
+                'first_name': self.request.user.first_name,
+                'last_name': self.request.user.last_name,
+            }
+        }
+        return Response(response)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+
+    serializer_class = serializers.ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
