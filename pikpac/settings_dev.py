12fd0f01a7acd270d13d982af6205931ecc85e8c
Brian Tiemann <btman@mac.com>
2019-07-14 08:54:36 -0400

    Fix thinksmall references in settings_dev

--- pikpac/settings_dev.py
+++ pikpac/settings_dev.py
@@ -8,15 +8,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'api.pikpac.grotto11.com']
 
-STATIC_ROOT = '/Users/brian.tiemann/Development/thinksmall/static_root'
+STATIC_ROOT = '/Users/brian.tiemann/Development/pikpac/static_root'
 
-MEDIA_ROOT = '/Users/brian.tiemann/Development/thinksmall/media'
+MEDIA_ROOT = '/Users/brian.tiemann/Development/pikpac/media'
 
 CELERY_TASK_ALWAYS_EAGER = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-SITE_EMAIL = 'info@thinksmall.com'
+SITE_EMAIL = 'info@pikpac.com'
 DEBUG_EMAIL = 'btman@mac.com'
 ADMIN_EMAIL = 'btman@mac.com'
 
